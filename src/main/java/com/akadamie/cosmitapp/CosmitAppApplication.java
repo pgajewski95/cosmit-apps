02d96fbdbbd0c072f5d10f75c2f26f7e2b29a4e1
pgajewski95 <pgajewski95@wp.pl>
2019-03-18 09:52:30 +0100

    init

---
+++ src/main/java/com/akadamie/cosmitapp/CosmitAppApplication.java
@@ -0,0 +1,13 @@
+package com.akadamie.cosmitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmitAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmitAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akadamie/cosmitapp/CosmitAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.akadamie.cosmitapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CosmitAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
